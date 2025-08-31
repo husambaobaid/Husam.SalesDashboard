69fbc22760a66274141547b59aeafd1eee1101a1
husambaobaid <hussambaobeid35@gmail.com>
2025-08-31 13:36:31 +0800

    Add project files.

---
+++ Husam.SalesDashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Husam.SalesDashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
