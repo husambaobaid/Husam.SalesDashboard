5c4a350f85e1d1b600ea14fb27eb05f0c631ef2e
husambaobaid <hussambaobeid35@gmail.com>
2025-08-31 15:08:00 +0800

    Step C: Scaffolded Customers & Products CRUD + navbar links

---
+++ Husam.SalesDashboard/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Husam.SalesDashboard.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Dashboard is live!";
+            return View();
+        }
+    }
+}
